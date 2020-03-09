aa466d6b815c1568c300245022d39e3458b117bb
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-03-09 15:03:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  counter = 0
+  while array[counter] do
+    new_array << array[counter] ** 2
+  end
+    return new_array
 end
+numbers = [2, 4, 6]
+square_array(numbers)
